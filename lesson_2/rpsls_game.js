27eecfc102cc182ad099889b4e9bed7131672e5b
Alex Simon <alexanderleesimon@gmail.com>
2021-04-30 15:33:47 -0400

    Third upload of JS101 4/30/21

---
+++ lesson_2/rpsls_game.js
@@ -0,0 +1,110 @@
+const readline = require('readline-sync');
+
+const VALID_CHOICES = ['rock', 'paper', 'scissors', 'lizard', 'Spock'];
+const FULL_CHOICES = ['(r)ock', '(p)aper', '(s)cissors', '(l)izard', '(S)pock'];
+
+let playerScore = 0;
+let computerScore = 0;
+
+function prompt (message) {
+  console.log(`=> ${message}`);
+}
+
+function displayWinner (choice, computerChoice) {
+  prompt(`You chose ${choice}, the computer chose ${computerChoice}`);
+
+  if ((choice === 'rock' && computerChoice === 'scissors') ||
+    (choice === 'rock' && computerChoice === 'lizard') ||
+    (choice === 'paper' && computerChoice === 'rock') ||
+    (choice === 'paper' && computerChoice === 'Spock') ||
+    (choice === 'scissors' && computerChoice === 'paper') ||
+    (choice === 'scissors' && computerChoice === 'lizard') ||
+    (choice === 'Spock' && computerChoice === 'scissors') ||
+    (choice === 'Spock' && computerChoice === 'rock') ||
+    (choice === 'lizard' && computerChoice === 'paper') ||
+    (choice === 'lizard' && computerChoice === 'Spock')) {
+    prompt('You won this round!');
+    return 'p';
+  } else if (choice === computerChoice) {
+    prompt('You tied this round!');
+    return 't';
+  } else {
+    prompt('You lost this round!');
+    return 'c';
+  }
+}
+
+function convertUserInput (choice) {
+  switch (choice) {
+    case 'r':
+      return 'rock';
+    case 'p':
+      return 'paper';
+    case 's':
+      return 'scissors';
+    case 'l':
+      return 'lizard';
+    case 'S':
+      return 'Spock';
+    default:
+      return choice;
+  }
+}
+
+function trackGrandWinner (match) {
+  if (match === 'p') {
+    playerScore += 1;
+  } else if (match === 'c') {
+    computerScore += 1;
+  }
+  if (playerScore === 5) {
+    prompt(`Player is the Grand Winner!!! Player won ${playerScore} to ${computerScore}`);
+    playerScore = 0;
+    computerScore = 0;
+  } else if (computerScore === 5) {
+    prompt(`Computer is the Grand Winner!!! The Computer won ${computerScore} to ${playerScore}`);
+    playerScore = 0;
+    computerScore = 0;
+  } else {
+    prompt(`The score is Player: ${playerScore}, Computer: ${computerScore}`);
+  }
+}
+
+prompt("Welcome to Rock, Paper, Scissors, Lizard, Spock!");
+prompt("------------------------------------------------");
+prompt("The Grand Winner is the first to 5 wins!");
+prompt("------------------------------------------------");
+
+while (true) {
+  prompt(`Choose one: ${FULL_CHOICES.join(', ')}`);
+  let choice = readline.question();
+
+  choice = convertUserInput(choice);
+
+
+  while (!VALID_CHOICES.includes(choice)) {
+    prompt("That's not a valid choice");
+    choice = readline.question();
+
+    choice = convertUserInput(choice);
+  }
+
+  let randomIndex = Math.floor(Math.random() * VALID_CHOICES.length);
+  let computerChoice = VALID_CHOICES[randomIndex];
+
+  trackGrandWinner(displayWinner(choice, computerChoice));
+
+  prompt('Play again? (y/n)');
+  let answer = readline.question().toLowerCase();
+  while (answer[0] !== 'n' && answer[0] !== 'y') {
+    prompt('Please enter "y" or "n".');
+    answer = readline.question().toLowerCase();
+  }
+  if (answer[0] === 'n') break;
+}
+
+if (playerScore > 0 || computerScore > 0) {
+  prompt(`The score was Player: ${playerScore}, Computer: ${computerScore}`);
+}
+
+prompt('Goodbye!');
