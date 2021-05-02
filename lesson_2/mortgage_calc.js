cb65de3fbf4bbc9b7939ae0247747bec7571746b
Alex Simon <alexanderleesimon@gmail.com>
2021-05-02 12:43:37 -0400

    Fourth uplpoad of JS101 5/2/21

--- lesson_2/mortgage_calc.js
+++ lesson_2/mortgage_calc.js
@@ -9,6 +9,53 @@ function converNum(stringNumber) {
   return Number(stringNumber);
 }
 
+function getLoan () {
+  prompt('What is your loan amount? (provide the number without commas; eg. $25000.00)');
+  let loanAmount = readline.question();
+  loanAmount = converNum(loanAmount);
+
+  while (!loanAmount || loanAmount < 0) {
+    prompt('Please re-enter a valid loan amount above 0 and without commas.');
+    loanAmount = readline.question();
+    loanAmount = converNum(loanAmount);
+  }
+  return loanAmount;
+}
+
+function getAPR () {
+  prompt('What is your annual percentage rate(APR)? (eg. 5%)');
+  let annualPercentageRate = readline.question();
+  let monthlyInterestRate = ((converNum(annualPercentageRate) / 100) / 12);
+
+  if (monthlyInterestRate === 0) {
+    return monthlyInterestRate;
+  } else {
+    while (!monthlyInterestRate || monthlyInterestRate < 0) {
+      prompt('Please re-enter a valid annual interest rate.');
+      annualPercentageRate = readline.question();
+      monthlyInterestRate = ((converNum(annualPercentageRate) / 100) / 12);
+      if (monthlyInterestRate === 0) {
+        return monthlyInterestRate;
+      }
+    }
+    return monthlyInterestRate;
+  }
+}
+
+function getMonthLoanDuration () {
+  prompt('What is your loan durations in years?');
+  let monthLoanDuration = readline.question();
+  monthLoanDuration = (converNum(monthLoanDuration));
+
+  while (!monthLoanDuration || !(Number.isInteger(monthLoanDuration)) ||
+    monthLoanDuration < 0) {
+    prompt('Please re-enter a valid loan duration in years above 0.');
+    monthLoanDuration = readline.question();
+    monthLoanDuration = (converNum(monthLoanDuration));
+  }
+  return monthLoanDuration * 12;
+}
+
 prompt('Welcome to the Mortgage Calculator!');
 
 let answer = 'y';
@@ -16,45 +63,29 @@ let answer = 'y';
 while (answer === 'y') {
   prompt('--------------------------------------');
 
-  prompt('What is your loan amount? (provide the number without commas; eg. $25000.00)');
-  let loanAmount = readline.question();
-  loanAmount = converNum(loanAmount);
+  let finalLoanAmount = getLoan();
 
-  while (!loanAmount) {
-    prompt('Please re-enter a valid loan amount above 0 and without commas.');
-    loanAmount = readline.question();
-    loanAmount = converNum(loanAmount);
+  let finalMonthlyInterestRate = getAPR();
+
+  let finalMonthLoanDuration = getMonthLoanDuration();
+
+  let monthlyPayment;
+  if (finalMonthlyInterestRate === 0) {
+    monthlyPayment = finalLoanAmount / finalMonthLoanDuration;
+    monthlyPayment = (Math.round(monthlyPayment * 100) / 100);
+  } else {
+    monthlyPayment =
+      finalLoanAmount * (finalMonthlyInterestRate /
+      (1 - Math.pow((1 + finalMonthlyInterestRate),
+        (-finalMonthLoanDuration))));
+    monthlyPayment = (Math.round(monthlyPayment * 100) / 100);
   }
-
-  prompt('What is your annual percentage rate(APR)? (eg. 5%)');
-  let annualPercentageRate = readline.question();
-  let monthlyInterestRate = ((converNum(annualPercentageRate) / 100) / 12);
-
-  while (!monthlyInterestRate) {
-    prompt('Please re-enter a valid annual interest rate above 0.');
-    annualPercentageRate = readline.question();
-    monthlyInterestRate = ((converNum(annualPercentageRate) / 100) / 12);
-  }
-
-  prompt('What is your loan durations in years? (eg. 2.5)');
-  let monthLoanDuration = readline.question();
-  monthLoanDuration = (converNum(monthLoanDuration)) * 12;
-
-  while (!monthLoanDuration) {
-    prompt('Please re-enter a valid loan duration in years above 0.');
-    monthLoanDuration = readline.question();
-    monthLoanDuration = (converNum(monthLoanDuration)) * 12;
-  }
-
-  let monthlyPayment =
-    loanAmount * (monthlyInterestRate /
-    (1 - Math.pow((1 + monthlyInterestRate), (-monthLoanDuration))));
-  monthlyPayment = (Math.round(monthlyPayment * 100) / 100);
 
   prompt('Your monthly payment is: $' + String(monthlyPayment) + '.');
 
   prompt('Need another calculation?');
   prompt('Please enter "y" or "n".');
   answer = readline.question().toLowerCase();
-  answer = answer[0];
 }
+
+prompt('Goodbye!');
