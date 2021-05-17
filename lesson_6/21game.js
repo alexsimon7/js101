da92e1c1398be3ec2c50b449839bb925d8d41692
Alex Simon <alexanderleesimon@gmail.com>
2021-05-17 15:51:23 -0400

    16th upload of JS101 5/17/21

--- lesson_6/21game.js
+++ lesson_6/21game.js
@@ -5,6 +5,14 @@ const FULL_DECK = [
   8, 8, 8, 8, 9, 9, 9, 9, 10, 10, 10, 10, 'J', 'J', 'J', 'J', 'Q', 'Q',
   'Q', 'Q', 'K', 'K', 'K', 'K', 'A', 'A', 'A', 'A'];
 
+const GAME_TO_AMOUNT = 21;
+const DEALER_HOLD_AMOUNT = 17;
+
+let playerTotal;
+let dealerTotal;
+let playerMatchScore = 0;
+let dealerMatchScore = 0;
+
 function prompt (message) {
   console.log(`=> ${message}`);
 }
@@ -13,7 +21,7 @@ function initializeDeck (deck) {
   let copyDeck = deck.slice();
   return copyDeck;
 }
- 
+
 function dealCard (deck, hand) {
   let currentCard = deck[Math.floor(Math.random() * (deck.length))];
   hand.push(currentCard);
@@ -21,153 +29,175 @@ function dealCard (deck, hand) {
   deck.splice(locationInDeck, 1);
 }
 
-function displayPlayerHand (hand) {
-  let displayHand = 'Player Hand: ';
-  for (index = 0; index < hand.length; index++) {
+function displayHand (hand, player) {
+  let displayHand = `${player}'s Hand: `;
+  for (let index = 0; index < hand.length; index++) {
     displayHand += `[${[hand[index]]}] `;
   }
   return displayHand;
 }
 
 function displayDealerHand (hand) {
-  let displayHand = 'Dealer Hand: ';
-  if (hand.length === 2) { //Bug - need to fix display of dealer hand after first hand
-    return displayHand += `[${[hand[0]]}] [?]`
-  } else {
-    for (index = 0; index < hand.length; index++) {
-      displayHand += `[${[hand[index]]}] `;
-    }
-    return displayHand;
-  }
+  let displayHand = "Dealer's Hand: ";
+  displayHand += `[${[hand[0]]}] [?]`;
+  return displayHand;
 }
 
 function totalHand (hand) {
   let copyHand = hand.slice();
-  
- //Numeralizes Hand 
+
   copyHand = copyHand.map(card => {
-    if (typeof(card) !== 'number') { //filter?
-      return (card === 'A' ? card = 11 : card = 10);
+    if (typeof (card) !== 'number') {
+      card = card === 'A' ? 11 : 10;
+      return card;
     } else {
-      return card = card;
+      return card;
     }
   });
- 
-//1. If hand includes Aces(11)
+
   if (copyHand.includes(11)) {
-    while (copyHand.reduce((sum, value) => sum + value) > 21) {
+    while (copyHand.reduce((sum, value) => sum + value) > GAME_TO_AMOUNT) {
       let locationInHand = copyHand.indexOf(11);
       copyHand[locationInHand] = 1;
       if (!(copyHand.includes(11))) break;
     }
-    return copyHand.reduce((sum, value) => sum + value);
+  }
+  return copyHand.reduce((sum, value) => sum + value);
+}
 
-  } else {
-//2. If hand does not includes Aces(11)     
-    return copyHand.reduce((sum, value) => sum + value);
+function playerInputCheck() {
+  let choice = readline.question().trim().toLowerCase();
+  while (choice !== 'h' && choice !== 's') {
+    prompt('Invalid entry. Hit or Stay? (h/s)');
+    choice = readline.question().trim().toLowerCase();
+  }
+  return choice;
+}
+
+
+function playerPlays (playerHand, gameDeck) {
+  while (true) {
+    playerTotal = totalHand(playerHand);
+    prompt('Hit or Stay? (h/s)');
+
+    let choice = playerInputCheck();
+
+    if (choice === 'h') {
+      dealCard(gameDeck, playerHand);
+      playerTotal = totalHand(playerHand);
+
+      prompt(displayHand(playerHand, 'Player'));
+
+      if (playerTotal > GAME_TO_AMOUNT) {
+        return playerTotal;
+      }
+    } else {
+      prompt(`You hold at ${playerTotal}.`);
+      prompt(displayHand(playerHand, 'Player'));
+      return playerTotal;
+    }
   }
 }
 
-function playRound () {
+function dealerPlays(playerHand, dealerHand, gameDeck) {
+  dealerTotal = totalHand(dealerHand);
+  while (dealerTotal < DEALER_HOLD_AMOUNT) {
+    prompt(`Dealer hits with ${dealerTotal}!`);
+    dealCard(gameDeck, dealerHand);
+    dealerTotal = totalHand(dealerHand);
 
-  prompt('Welcome to 21! Good Luck!');
+    prompt(displayHand(dealerHand, 'Dealer'));
+    prompt('---------------');
+  }
+  if (dealerTotal <= GAME_TO_AMOUNT) {
+    prompt(`Dealer holds at ${dealerTotal}.`);
+    prompt('---------------');
+    prompt(displayHand(playerHand, 'Player'));
+    prompt(displayHand(dealerHand, 'Dealer'));
+    return dealerTotal;
+  } else {
+    return dealerTotal;
+  }
+}
 
-  //1.Initialize Deck
+
+function displayResult(playerTotal, dealerTotal) {
+  if (playerTotal > GAME_TO_AMOUNT) {
+    prompt(`Dealer wins! Player busted with ${playerTotal}!`);
+    tallyMatch('Dealer');
+  } else if (playerTotal < 22 && dealerTotal > GAME_TO_AMOUNT) {
+    prompt(`Dealer busted with ${dealerTotal}! Player wins!`);
+    tallyMatch('Player');
+  } else if (playerTotal === dealerTotal) {
+    prompt(`Player and Dealer push at ${playerTotal}!`);
+  } else if (playerTotal > dealerTotal) {
+    prompt(`Player wins with ${playerTotal}!`);
+    tallyMatch('Player');
+  } else {
+    prompt(`Dealer wins with ${dealerTotal}!`);
+    tallyMatch('Dealer');
+  }
+  prompt('---------------');
+}
+function playAgain() {
+  prompt('Play another hand? (y/n)');
+
+  let answer = readline.question().trim().toLowerCase();
+
+  while (answer !== 'n' && answer !== 'y') {
+    prompt('Invalid entry. Play another hand? (y/n)');
+    answer = readline.question().trim().toLowerCase();
+  }
+  return answer;
+}
+
+function tallyMatch(winner) {
+  if (winner === 'Player') {
+    playerMatchScore += 1;
+  } else if (winner === 'Dealer') {
+    dealerMatchScore += 1;
+  }
+}
+
+function displayMatchResults() {
+  if (playerMatchScore === 5) {
+    prompt(`Player has won the match ${playerMatchScore} to ${dealerMatchScore}!`);
+    playerMatchScore = 0;
+    dealerMatchScore = 0;
+  } else if (dealerMatchScore === 5) {
+    prompt(`Dealer has won the match ${dealerMatchScore} to ${playerMatchScore}!`);
+    playerMatchScore = 0;
+    dealerMatchScore = 0;
+  } else {
+    prompt(`Current Match Score: Player ${playerMatchScore}, Dealer ${dealerMatchScore}!`);
+  }
+}
+
+prompt('Welcome to 21! Good Luck!');
+
+while (true) {
   let gameDeck = initializeDeck(FULL_DECK);
 
-  //2. Deal cards to player and dealer
   let playerHand  = [];
   let dealerHand = [];
 
-  //2a. Select cards from game deck and put in players hand, update game deck
-  dealCard(gameDeck, playerHand);  
+  dealCard(gameDeck, playerHand);
   dealCard(gameDeck, dealerHand);
   dealCard(gameDeck, playerHand);
   dealCard(gameDeck, dealerHand);
 
-  //2b. Display cards to player
-  prompt(displayPlayerHand(playerHand));
+  prompt(displayHand(playerHand, 'Player'));
   prompt(displayDealerHand(dealerHand));
 
-  //3. Player turn: hit or stay (repeat until bust or stay)
+  playerTotal = playerPlays(playerHand, gameDeck);
 
-  while (true) {
-
-  //3a. Prompt player to hit or stay
-    prompt('Hit or Stay? (h/s)');
-
-  //3b. Take input from player (and check for valid)
-    let choice = readline.question().trim().toLowerCase();
-    while (choice !== 'h' && choice !== 's') {
-      prompt('Invalid entry. Hit or Stay? (h/s)');
-      choice = readline.question().trim().toLowerCase();
-    }
-
-  //3c. If hit add card, stay (nada)
-    if(choice === 'h') {
-      dealCard(gameDeck, playerHand);
-    
-      prompt(displayPlayerHand(playerHand));
-      prompt(displayDealerHand(dealerHand));
-
-  //3d. Check for bust/if player busts, dealer wins
-      if (totalHand(playerHand) > 21) {
-        prompt(`You bust!Your cards total ${totalHand(playerHand)}.`)
-        break;
-      }
-    } else {
-      prompt(`You hold at ${totalHand(playerHand)}.`);
-      break;
-    }
+  if (playerTotal < 22) {
+    dealerTotal = dealerPlays(playerHand, dealerHand, gameDeck, playerTotal);
   }
+  displayResult(playerTotal, dealerTotal);
+  displayMatchResults();
 
-  //5. Dealer turn: hit or stay (repeat until total >= 17)
-  if (totalHand(playerHand) < 22) {
-    while (totalHand(dealerHand) < 17) {
-      prompt(`Dealer hits at ${totalHand(dealerHand)}!`);
-      dealCard(gameDeck, dealerHand);
+  let answer = playAgain();
 
-      prompt(displayDealerHand(dealerHand));
-      prompt('---------------');
-
-      if (totalHand(dealerHand) > 21) {
-        prompt(`Dealer busts! Dealer's cards total ${totalHand(dealerHand)}.`)
-        break;
-      }
-    }
-    if (totalHand(dealerHand) <= 21) {
-      prompt(`Dealer holds at ${totalHand(dealerHand)}.`);
-      prompt('---------------');
-      prompt(displayPlayerHand(playerHand));
-      prompt(displayDealerHand(dealerHand));
-    }  
-  }  
-
-  if (totalHand(playerHand) > 21) {
-    prompt(`Dealer wins! Player is busted!`);
-  } else if (totalHand(playerHand) < 22 && totalHand(dealerHand) > 21) {
-    prompt('Dealer busted! Player wins!');
-  } else if (totalHand(playerHand) === totalHand(dealerHand)) {
-    prompt(`Player and Dealer push at ${totalHand(playerHand)}!`);
-  } else {
-    if (totalHand(playerHand) > totalHand(dealerHand)) {
-      prompt(`Player wins with ${totalHand(playerHand)}!`);
-    } else {
-      prompt(`Dealer wins with ${totalHand(dealerHand)}!`);
-    } 
-  }
-
-
-
-
-
-  //6. If dealer busts, player wins
-
-
-
-
-
-  //7. Compare cards and declare winner
+  if (answer === 'n') break;
 }
-
-playRound();
