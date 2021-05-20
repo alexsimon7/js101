0cef3a4c10c71acf614e0207b8e6c582524ba7f7
asimon <alexanderleesimon@gmail.com>
2021-05-20 12:23:00 -0400

    17th upload of JS101 5/20/21

--- lesson_6/21game.js
+++ lesson_6/21game.js
@@ -1,12 +1,28 @@
 const readline = require('readline-sync');
 
-const FULL_DECK = [
-  2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 6, 6, 6, 6, 7, 7, 7, 7,
-  8, 8, 8, 8, 9, 9, 9, 9, 10, 10, 10, 10, 'J', 'J', 'J', 'J', 'Q', 'Q',
-  'Q', 'Q', 'K', 'K', 'K', 'K', 'A', 'A', 'A', 'A'];
+const SPADE = String.fromCharCode(9824);
+const HEART = String.fromCharCode(9829);
+const CLUB = String.fromCharCode(9827);
+const DIAMOND = String.fromCharCode(9830);
 
-const GAME_TO_AMOUNT = 21;
-const DEALER_HOLD_AMOUNT = 17;
+const FULL_DECK = [
+  [2, SPADE], [2, HEART], [2, CLUB], [2, DIAMOND], [3, SPADE],
+  [3, HEART], [3, CLUB], [3, DIAMOND], [4, SPADE], [4, HEART],
+  [4, CLUB], [4, DIAMOND], [5, SPADE], [5, HEART], [5, CLUB],
+  [5, DIAMOND], [6, SPADE], [6, HEART], [6, CLUB], [6, DIAMOND],
+  [7, SPADE], [7, HEART], [7, CLUB], [7, DIAMOND], [8, SPADE],
+  [8, HEART], [8, CLUB], [8, DIAMOND], [9, SPADE], [9, HEART],
+  [9, CLUB], [9, DIAMOND], [10, SPADE], [10, HEART], [10, CLUB],
+  [10, DIAMOND], ['J', SPADE], ['J', HEART], ['J', CLUB],
+  ['J', DIAMOND], ['Q', SPADE], ['Q', HEART], ['Q', CLUB],
+  ['Q', DIAMOND], ['K', SPADE], ['K', HEART], ['K', CLUB],
+  ['K', DIAMOND], ['A', SPADE], ['A', HEART], ['A', CLUB],
+  ['A', DIAMOND]];
+
+const TWENTY_ONE = 21;
+const DEALER_LIMIT = 17;
+const ACE_HIGH_VALUE = 11;
+const FACE_VALUE = 10;
 
 let playerTotal;
 let dealerTotal;
@@ -32,34 +48,35 @@ function dealCard (deck, hand) {
 function displayHand (hand, player) {
   let displayHand = `${player}'s Hand: `;
   for (let index = 0; index < hand.length; index++) {
-    displayHand += `[${[hand[index]]}] `;
+    displayHand += `[${[hand[index][0]]}${[hand[index][1]]}] `;
   }
   return displayHand;
 }
 
 function displayDealerHand (hand) {
   let displayHand = "Dealer's Hand: ";
-  displayHand += `[${[hand[0]]}] [?]`;
+  displayHand += `[${[hand[0][0]]}${[hand[0][1]]}] [?]`;
   return displayHand;
 }
 
 function totalHand (hand) {
   let copyHand = hand.slice();
+  copyHand = copyHand.reduce((acc, val) => acc.concat(val), []).filter(card => typeof (card) === 'number' || card === 'J' || card === 'Q' || card === 'K' || card === 'A');
 
   copyHand = copyHand.map(card => {
     if (typeof (card) !== 'number') {
-      card = card === 'A' ? 11 : 10;
+      card = card === 'A' ? ACE_HIGH_VALUE : FACE_VALUE;
       return card;
     } else {
       return card;
     }
   });
 
-  if (copyHand.includes(11)) {
-    while (copyHand.reduce((sum, value) => sum + value) > GAME_TO_AMOUNT) {
-      let locationInHand = copyHand.indexOf(11);
+  if (copyHand.includes(ACE_HIGH_VALUE)) {
+    while (copyHand.reduce((sum, value) => sum + value) > TWENTY_ONE) {
+      let locationInHand = copyHand.indexOf(ACE_HIGH_VALUE);
       copyHand[locationInHand] = 1;
-      if (!(copyHand.includes(11))) break;
+      if (!(copyHand.includes(ACE_HIGH_VALUE))) break;
     }
   }
   return copyHand.reduce((sum, value) => sum + value);
@@ -88,7 +105,7 @@ function playerPlays (playerHand, gameDeck) {
 
       prompt(displayHand(playerHand, 'Player'));
 
-      if (playerTotal > GAME_TO_AMOUNT) {
+      if (playerTotal > TWENTY_ONE) {
         return playerTotal;
       }
     } else {
@@ -101,7 +118,7 @@ function playerPlays (playerHand, gameDeck) {
 
 function dealerPlays(playerHand, dealerHand, gameDeck) {
   dealerTotal = totalHand(dealerHand);
-  while (dealerTotal < DEALER_HOLD_AMOUNT) {
+  while (dealerTotal < DEALER_LIMIT) {
     prompt(`Dealer hits with ${dealerTotal}!`);
     dealCard(gameDeck, dealerHand);
     dealerTotal = totalHand(dealerHand);
@@ -109,7 +126,7 @@ function dealerPlays(playerHand, dealerHand, gameDeck) {
     prompt(displayHand(dealerHand, 'Dealer'));
     prompt('---------------');
   }
-  if (dealerTotal <= GAME_TO_AMOUNT) {
+  if (dealerTotal <= TWENTY_ONE) {
     prompt(`Dealer holds at ${dealerTotal}.`);
     prompt('---------------');
     prompt(displayHand(playerHand, 'Player'));
@@ -122,10 +139,10 @@ function dealerPlays(playerHand, dealerHand, gameDeck) {
 
 
 function displayResult(playerTotal, dealerTotal) {
-  if (playerTotal > GAME_TO_AMOUNT) {
+  if (playerTotal > TWENTY_ONE) {
     prompt(`Dealer wins! Player busted with ${playerTotal}!`);
     tallyMatch('Dealer');
-  } else if (playerTotal < 22 && dealerTotal > GAME_TO_AMOUNT) {
+  } else if (playerTotal < 22 && dealerTotal > TWENTY_ONE) {
     prompt(`Dealer busted with ${dealerTotal}! Player wins!`);
     tallyMatch('Player');
   } else if (playerTotal === dealerTotal) {
@@ -198,6 +215,7 @@ while (true) {
   displayMatchResults();
 
   let answer = playAgain();
+  console.clear();
 
   if (answer === 'n') break;
 }
